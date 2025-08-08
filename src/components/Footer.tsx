8734919306594cd313c1b80574d7a6569c2cc956
May <x.may.bar@gmail.com>
2025-08-08 13:22:51 +0300

    creating footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{padding:'12px', borderTop:'1px solid #eee', marginTop:24}}>
+      <small>© 2025 Ticket Management</small>
+    </footer>
+  );
+}

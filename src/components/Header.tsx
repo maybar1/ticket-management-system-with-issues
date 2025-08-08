f781d2fd5652dfa0998f7bcf9489ee05b1d5ab95
May <x.may.bar@gmail.com>
2025-08-08 13:22:41 +0300

    creating header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header style={{padding:'12px', borderBottom:'1px solid #eee'}}>
+      <h1>Ticket Management System</h1>
+    </header>
+  );
+}
+

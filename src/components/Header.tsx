13a7efe2c728654a4a13d015c70b7d5ce1d0ff67
vered asaraf <veredd570@gmail.com>
2025-08-19 20:22:33 +0300

    Update Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,27 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { Link as RouterLink } from 'react-router-dom';
+
 export default function Header() {
   return (
-    <header style={{padding:'12px', borderBottom:'1px solid #eee'}}>
-      <h1>Ticket Management System</h1>
-    </header>
+    <AppBar position="static">
+      <Toolbar>
+        {/* כותרת בצד שמאל */}
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Ticket Management System
+        </Typography>
+
+        {/* קישורי ניווט */}
+        <Box sx={{ display: 'flex', gap: 1 }}>
+          <Button color="inherit" component={RouterLink} to="/">בית</Button>
+          <Button color="inherit" component={RouterLink} to="/forms">טפסים</Button>
+          <Button color="inherit" component={RouterLink} to="/management">ניהול</Button>
+          <Button color="inherit" component={RouterLink} to="/help">עזרה</Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
-
